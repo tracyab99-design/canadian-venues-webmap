f4712cbbc3363759cdabf8868c272b0c91bebc1a
tracyab99-design <tracyab99@gmail.com>
2026-04-27 11:05:18 -0600

    Add OpenStreetMap tile layer to the map
    Fix syntax error in tileLayer method call.

--- js/map.js
+++ js/map.js
@@ -1,1 +1,8 @@
+// Create the map in the 'map' div
+const map = L.map('map').setView([56.1304, -106.3468], 4);
 
+// Add an OpenStreetMap tile layer (basemap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map
