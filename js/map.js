859dcf6f8ad664cbcc3c80489581b057f2107fc2
tracyab99-design <tracyab99@gmail.com>
2026-04-27 11:26:27 -0600

    Fix attribution formatting in map.js

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
+```js
 // Create the map inside the div with id="map"
 const map = L.map('map').setView([56.1304, -106.3468], 4);
 
 // Add an OpenStreetMap basemap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; OpenStreetMap contributors'
+  attribution: '\u00A9 OpenStreetMap contributors'
 }).addTo(map);
 
 // Optional test marker
 L.marker([51.0447, -114.0719]).addTo(map)
   .bindPopup('Hello from Calgary 👋');
-``
