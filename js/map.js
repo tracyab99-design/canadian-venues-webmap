a33625a675041c2b4cec4bc1738934f0873d8c77
tracyab99-design <tracyab99@gmail.com>
2026-04-27 11:29:41 -0600

    Update map center and marker popup text

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 ```js
 // Create the map inside the div with id="map"
-const map = L.map('map').setView([56.1304, -106.3468], 4);
+const map = L.map('map').setView([43.6532, -79.3832], 6);
 
 // Add an OpenStreetMap basemap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,4 +10,4 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Optional test marker
 L.marker([51.0447, -114.0719]).addTo(map)
-  .bindPopup('Hello from Calgary 👋');
+  .bindPopup('Toronto');
