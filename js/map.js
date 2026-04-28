fa70711f045a3e5df411e334c60f430c7ced1801
tracyab99-design <tracyab99@gmail.com>
2026-04-28 09:18:57 -0600

    Increase map zoom level from 6 to 8

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 // Create the map inside the div with id="map"
-const map = L.map('map').setView([43.6532, -79.3832], 6);
+const map = L.map('map').setView([43.6532, -79.3832], 8);
 
 // Add an OpenStreetMap basemap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
